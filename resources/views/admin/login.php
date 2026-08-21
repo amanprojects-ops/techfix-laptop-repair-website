f6b5c483e0146de6b63ce7db2076980f3c5b0ad4
amanprojects-ops <fullstackdeveloper014@gmail.com>
2026-08-21 09:56:29 +0530

    fix(admin-icons): restore FontAwesome icon rendering by removing universal font-family override

--- resources/views/admin/login.php
+++ resources/views/admin/login.php
@@ -17,8 +17,8 @@ $flash_error = $flash_error ?? Session::getFlash('error');
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
 
-  <!-- Font Awesome 6.4 -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+  <!-- Font Awesome 6.5 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   
   <!-- Stylesheets -->
   <link rel="stylesheet" href="/admin-assets/css/styles.css" />
